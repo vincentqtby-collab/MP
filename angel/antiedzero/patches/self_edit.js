eb69041e9556a50356b5eb9eb506ed786e06fb3b
vincentqtby-collab <vincentqtby@gmail.com>
2026-07-08 22:17:50 +0800

    Update self_edit.js

--- angel/antiedzero/patches/self_edit.js
+++ angel/antiedzero/patches/self_edit.js
@@ -1,19 +1,18 @@
 import { before } from "@vendetta/patcher";
-import { findByProps } from '@vendetta/metro';
+import { findByProps } from "@vendetta/metro";
 import { regexEscaper, isEnabled } from "..";
 
-const Message = findByProps("sendMessage", "startEditMessage")
+const Message = findByProps("sendMessage", "startEditMessage");
 
-export default () => before('startEditMessage', Message, (args) => {
-	if(!isEnabled) return;
+export default () => before("startEditMessage", Message, args => {
+    if (!isEnabled) return;
 
-	const DAN = regexEscaper("`[ EDITED ]`\n\n")
+    const [, , msg] = args;
+    if (typeof msg !== "string") return;
 
-	const regexPattern = new RegExp(DAN, 'gmi');
+    const DAN = regexEscaper("`[ EDITED ]`\n\n");
+    const regexPattern = new RegExp(DAN, "gmi");
 
-	const [, , msg] = args;
-	const lats = msg.split(regexPattern);
-	const f = lats[lats.length - 1];
-
-	args[2] = f;
+    const lats = msg.split(regexPattern);
+    args[2] = lats[lats.length - 1];
 });
